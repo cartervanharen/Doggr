2f9c3516efe388b14220780830d064472f21d17a
Carter <carter.vanharen@gmail.com>
2024-07-22 11:13:31 -0700

    internalOps.js fully refactored

--- backend/routes/internalOps.js
+++ backend/routes/internalOps.js
@@ -1,18 +1,24 @@
-/**
+/** 
+ *         ____                                  
+ *        / __ \  ____    ____ _   ____ _   _____
+ *       / / / / / __ \  / __ `/  / __ `/  / ___/
+ *      / /_/ / / /_/ / / /_/ /  / /_/ /  / /    
+ *     /_____/  \____/  \__, /   \__, /  /_/     
+ *                     /____/   /____/           
+ *
  * @fileoverview This file contains the router definitions for internal API endpoints that manage user data.
  * These endpoints are used primarily for administrative tasks such as managing user accounts, interactions, and testing.
  * These routes are intended for use by system administrators and should ideally be protected by appropriate authentication mechanisms.
-
+ * 
  * @author Carter VanHaren
- * @date 2024-07-22
  */
- 
+
 import express from "express";
 import { supabase } from "../utils/supabaseClient.js";
 const router = express.Router();
 
 /**
- * Gets a list of all users.
+ * Gets a list of all users in the user table.
  * @route GET /get-all-users
  * @returns {Object[]} data - Array of all user records.
  * @returns {Error} 500 - Bubbled up error object with message.
@@ -26,7 +32,7 @@ router.get("/get-all-users", async (req, res) => {
 });
 
 /**
- * Gets a list of all user UUIDs.
+ * Gets a list of all user UUIDs in the users table.
  * @route GET /get-all-uuid
  * @returns {Object[]} data - Array of user UUIDs.
  * @returns {Error} 500 - Bubbled up error object with message.
@@ -40,7 +46,7 @@ router.get("/get-all-uuid", async (req, res) => {
 });
 
 /**
- * Gets a list of all user data entries.
+ * Gets a list of all user data entries in the userdata table.
  * @route GET /get-all-userdata
  * @returns {Object[]} data - Array of all user data records.
  * @returns {Error} 500 - Bubbled up error object with message.
@@ -54,7 +60,12 @@ router.get("/get-all-userdata", async (req, res) => {
 });
 
 /**
- * Fetches user data based on the provided access token.
+ * Fetches user data based on the provided access token
+ *
+ * How it works:
+ * 1. Fetches the UUID from the auth table
+ * 2. Selects the record from the userdata table via UUID.
+ *
  * @route POST /userdata
  * @param {string} req.body.accessToken - Access token to authenticate and identify the user.
  * @returns {Object} user - User data for the authenticated user.
@@ -84,7 +95,7 @@ router.post("/userdata", async (req, res) => {
 });
 
 /**
- * Fetches all relationship entries.
+ * Fetches all relationship entries in the relation table.
  * @route GET /get-all-relation
  * @returns {Object[]} data - Array of all relation records.
  * @returns {Error} 500 - Bubbled up error object with message.
@@ -97,9 +108,17 @@ router.get("/get-all-relation", async (req, res) => {
   res.json(data);
 });
 
-
 /**
  * Updates the location data for all users based on their address.
+ *
+ * How it works:
+ * 1. Fetches all users' UUIDs and addresses from the database.
+ * 2. For each user with a valid address, encodes the address and constructs a URL to query the PositionStack API.
+ * 3. Sends a request to the PositionStack API to convert the encoded address into latitude and longitude.
+ * 4. Collects the latitude and longitude data for each user and prepares bulk updates.
+ * 5. Performs an upsert operation to update the userdata table with the new location data for each user.
+ * 6. Returns a success message with the count of updated records or an error if the operation fails.
+ *
  * @route GET /update-all-locations
  * @returns {string} message - Success message with count of updated records.
  * @returns {Error} 500 - Error during fetching users or updating their location.
@@ -137,7 +156,7 @@ router.get("/update-all-locations", async (req, res) => {
 });
 
 /**
- * Manually adds an interaction record between two users.
+ * Manually adds an interaction record between two users in the relation table.
  * @route POST /manual-add-interaction
  * @param {string} req.body.user_from - UUID of the user initiating the interaction.
  * @param {string} req.body.user_to - UUID of the recipient user.
@@ -168,7 +187,7 @@ router.post("/manual-add-interaction", async (req, res) => {
 });
 
 /**
- * Fetches user information by UUID.
+ * Fetches user information by UUID from the userdata table.
  * @route POST /user-info-byuuid
  * @param {string} req.body.uuid - UUID of the user to fetch.
  * @returns {Object} user - User data for the specified UUID.
@@ -193,7 +212,7 @@ router.post("/user-info-byuuid", async (req, res) => {
 });
 
 /**
- * Generates new next-users entries for the specified user.
+ * Generates new next-users entries for the specified user using the matchClosestUsers function.
  * @route POST /generate-new-nextusers
  * @param {string} req.body.userid - UUID of the user for whom to generate entries.
  * @returns {string} worked - Confirmation message if successful.
