8198e4ef5a0bea27935e1e67fa1b676600d47d13
Carter <carter.vanharen@gmail.com>
2024-07-25 14:38:20 -0700

    Message Test

---
+++ doggr/src/Tests/MessageTest.jsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+async function MessageTest() {
+  try {
+    const response = await axios.get(
+      `http://localhost:3000/messages?user_from=da58ef62-ab09-4b6c-8989-23e6a1896904&user_to=ca3afab4-d225-4cbc-b25a-f21854ce980d`
+    );
+    console.log(response.data);
+    return ["Messages Received Properly", "Passing"];
+  } catch (error) {
+    console.error(
+      "Error Finding Matches:",
+      error.response ? error.response.data : error.message
+    );
+    const errorMessage = error.response
+      ? JSON.stringify(error.response.data)
+      : error.message;
+    return ["MESSAGING ERROR:" + errorMessage, "Failed"];
+  }
+}
+export default MessageTest;
+
